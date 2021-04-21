e35775fa634c066359fb8f062ee4b62999934370
ritresh <ritresh.girdhar@loblaw.ca>
2021-04-21 15:24:32 +0530

    CDC cleaning

--- invoice-service/src/main/java/learning/personal/cdc/demo/listener/EventListener.java
+++ invoice-service/src/main/java/learning/personal/cdc/demo/listener/EventListener.java
@@ -4,7 +4,12 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service public class EventListener {
-	@KafkaListener(id = "myId", topics = "topic1") public void listen(String in) {
+
+	@KafkaListener(id = "myId", topics = "dbserver1.inventory.customers")
+	public void listen(String in) {
+
+
 		System.out.println(in);
 	}
+
 }
